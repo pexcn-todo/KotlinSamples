483c1815b694205b4cf2fe46d6f6bb470261c0e4
pexcn <pexcn97@gmail.com>
2017-05-15 02:54:06 +0000

    Revert "test"
    This reverts commit b59537cbed54b74c17872ce5570ce6e2869723e0.

--- app/src/main/kotlin/me/pexcn/kotlin/samples/activity/Test2Activity.kt
+++ app/src/main/kotlin/me/pexcn/kotlin/samples/activity/Test2Activity.kt
@@ -3,7 +3,6 @@ package me.pexcn.kotlin.samples.activity
 import me.pexcn.kotlin.samples.R
 import me.pexcn.kotlin.samples.base.BaseActivity
 
-
 /**
  * Created by pexcn on 2017-05-03.
  */
@@ -15,10 +14,9 @@ class Test2Activity : BaseActivity() {
     override fun init() {
         super.init()
 
-
     }
 
     override fun isSubActivity(): Boolean {
-        return false
+        return true
     }
 }
--- app/src/main/kotlin/me/pexcn/kotlin/samples/activity/TestActivity.kt
+++ app/src/main/kotlin/me/pexcn/kotlin/samples/activity/TestActivity.kt
@@ -1,76 +1,23 @@
 package me.pexcn.kotlin.samples.activity
 
-import android.support.v4.view.PagerAdapter
-import android.support.v4.view.ViewPager
-import android.util.SparseArray
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import me.pexcn.kotlin.samples.R
 import me.pexcn.kotlin.samples.base.BaseActivity
-import java.util.*
 
 /**
  * Created by pexcn on 2017-05-03.
  */
 class TestActivity : BaseActivity() {
     override fun getLayoutId(): Int {
-        return R.layout.test
+        return R.layout.activity_main
     }
 
     override fun init() {
         super.init()
 
-        var datas = SparseArray<Int>()
 
-        var oldList = Arrays.asList(0, 1, 1, 0, 0, 0, 0, 1, 0, 1,0,0,0,0,0,0)
-
-        var leftIndex = 0
-        var rightIndex = 1
-        for (i in oldList){
-            if (i == 0) {
-                datas.put(leftIndex,i)
-                leftIndex+=2
-            }else {
-                datas.put(rightIndex,i)
-                rightIndex +=2
-            }
-        }
-
-        println(datas)
-
-        var count = datas.size()
-
-        for (i in 0..count){
-            println("$i >> ${datas[i]}")
-        }
-
-
-        val viewPager: ViewPager = findViewById(R.id.view_pager) as ViewPager
-        viewPager.adapter = pagerAdapter
     }
 
     override fun isSubActivity(): Boolean {
         return true
-    }
-
-    val pagerAdapter = object : PagerAdapter() {
-        override fun isViewFromObject(view: View?, obj: Any?): Boolean {
-            return view === obj
-        }
-
-        override fun getCount(): Int = 10
-
-        override fun instantiateItem(container: ViewGroup?, position: Int): Any {
-            val view = LayoutInflater.from(container?.context).inflate(R.layout.viewpager, container, false)
-            container?.addView(view)
-            return view
-        }
-
-        override fun destroyItem(container: ViewGroup?, position: Int, obj: Any?) {
-            if (obj is View) {
-                container?.removeView(obj)
-            }
-        }
     }
 }
